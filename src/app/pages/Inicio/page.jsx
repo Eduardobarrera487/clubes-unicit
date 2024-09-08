b733c811c9024c3206b1bc71e24ee922989faafc
Eduardobarrera487 <barreraeduardo099@gmail.com>
2024-09-08 16:14:05 -0600

    Cambiados elementos en menucard y en la pagina de inicio

--- src/app/pages/Inicio/page.jsx
+++ src/app/pages/Inicio/page.jsx
@@ -2,9 +2,12 @@ import MenuCard from "@/app/_components/menuCard";
 import HeaderLogin from "@/app/_components/header";
 function Page() {
     return (
-        <div className="mx-8 my-8">
+        <div className="">
+            <div className="mx-8 my-8 grid grid-cols-3 gap-0">
+                <MenuCard/>
+                <div className="bg-red-500 w-full">Prueba aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssssssssssss</div>
+            </div>
             
-            <MenuCard/>
         </div>
     );
 }
