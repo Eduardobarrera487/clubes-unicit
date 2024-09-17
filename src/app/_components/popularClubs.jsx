eb0b5071ecb59435cf19d4cdabccecc457e3df5a
Eduardobarrera487 <barreraeduardo099@gmail.com>
2024-09-16 21:03:27 -0600

    Añadido cambios para frontend de mis clubes

--- src/app/_components/popularClubs.jsx
+++ src/app/_components/popularClubs.jsx
@@ -5,7 +5,7 @@ function PopularClubs() {
   return (
     <div className="h-full p-3 space-y-2 w-60 dark:bg-gray-50 dark:text-gray-800  rounded-md shadow-lg">
         <div className="mb-6 bg-[#FFDF37] text-[#274790] font-bold text-center py-3 rounded-t-lg">
-            <h2>Clubes populares</h2>
+            <h2>Mis Clubes</h2>
         </div>
         <div className="divide-y dark:divide-gray-300">
             <ul className="pt-2 pb-4 space-y-1 text-sm">
