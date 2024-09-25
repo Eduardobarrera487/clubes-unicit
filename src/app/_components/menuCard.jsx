3eb1a8b2719c473e705939f1a6a3098c0ef3a4dc
BradlyGutierrez <bradly.gutierrez14520@est.uca.edu.ni>
2024-09-24 20:53:29 -0600

    Cosas

--- src/app/_components/menuCard.jsx
+++ src/app/_components/menuCard.jsx
@@ -92,7 +92,7 @@ function MenuCard() {
     return (
         <div className="h-full space-y-2 w-60 dark:bg-gray-50 dark:text-gray-800 rounded-md shadow-lg">
             <div className="flex items-center p-2 space-x-4">
-                <img src="Foto del usuario" alt="" className="w-12 h-12 rounded-full dark:bg-gray-500" />
+                <img  src={`http://localhost:8000/${user.picture}`} alt="" className="w-12 h-12 rounded-full dark:bg-gray-500" />
                 <div>
                     <h2 className="text-lg font-semibold">{user.username}</h2>
                 </div>
